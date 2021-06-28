ac3dc741dc4ba9acb91f6a7fbc021ba05bfd82d2
Breens-Thomas <mbakabreens@gmail.com>
2021-06-28 11:24:14 +0300

    Moved to Onboarding activity after a few seconds with a fade in to fade out transition

--- app/src/main/java/com/example/dozetracker/ui/SplashScreen.java
+++ app/src/main/java/com/example/dozetracker/ui/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.dozetracker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.dozetracker.R;
 
@@ -12,5 +14,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        fadeInTransition();
+    }
+
+    private void fadeInTransition() {
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, OnboardingScreen.class);
+            startActivity(intent);
+            overridePendingTransition(R.transition.fade_in,R.transition.fade_out);
+        },3000);
     }
 }
