feff12bdf3581b99fdfa2b6c86aa2a58478b90c0
Breens-Thomas <mbakabreens@gmail.com>
2021-06-29 10:17:09 +0300

    Found a reference to the get started button

--- app/src/main/java/com/example/dozetracker/ui/OnboardingScreen.java
+++ app/src/main/java/com/example/dozetracker/ui/OnboardingScreen.java
@@ -2,15 +2,23 @@ package com.example.dozetracker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.dozetracker.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class OnboardingScreen extends AppCompatActivity {
+    @BindView(R.id.button) Button mGetStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_screen);
+        ButterKnife.bind(this);
     }
 }
