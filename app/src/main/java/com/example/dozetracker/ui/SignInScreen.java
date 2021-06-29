f91b6731061e823c31bf51a4590ebc4bda1f9957
Breens-Thomas <mbakabreens@gmail.com>
2021-06-29 13:37:20 +0300

    User can login with google and facebook account

--- app/src/main/java/com/example/dozetracker/ui/SignInScreen.java
+++ app/src/main/java/com/example/dozetracker/ui/SignInScreen.java
@@ -1,16 +1,37 @@
 package com.example.dozetracker.ui;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 import com.example.dozetracker.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SignInScreen extends AppCompatActivity {
+    @BindView(R.id.textView10) TextView mRegisterAccountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
+        ButterKnife.bind(this);
+        changePartOfTextViewColor();
+    }
+
+    private void changePartOfTextViewColor() {
+        String text = "Not registered? Create a new account";
+        SpannableString spannableString = new SpannableString(text);
+        ForegroundColorSpan foregroundColorSpanPurple = new ForegroundColorSpan(Color.rgb(51,116,229));
+
+        spannableString.setSpan(foregroundColorSpanPurple, 16, 36, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mRegisterAccountTextView.setText(spannableString);
     }
 }
