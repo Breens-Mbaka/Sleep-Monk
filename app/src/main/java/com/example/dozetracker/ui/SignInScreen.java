58e41a179df740184464872a79bf5314a502aa00
Breens-Thomas <mbakabreens@gmail.com>
2021-06-29 14:27:59 +0300

    Created sign up form for new user and user is able to navigate from sign in page to sign up page

--- app/src/main/java/com/example/dozetracker/ui/SignInScreen.java
+++ app/src/main/java/com/example/dozetracker/ui/SignInScreen.java
@@ -3,11 +3,13 @@ package com.example.dozetracker.ui;
 import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.dozetracker.R;
@@ -22,16 +24,26 @@ public class SignInScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
+
         ButterKnife.bind(this);
         changePartOfTextViewColor();
+        registerAccount();
+    }
+
+    private void registerAccount() {
+        mRegisterAccountTextView.setOnClickListener(v -> {
+            Intent intent = new Intent(SignInScreen.this,SignUpScreen.class);
+            startActivity(intent);
+        });
     }
 
     private void changePartOfTextViewColor() {
         String text = "Not registered? Create a new account";
         SpannableString spannableString = new SpannableString(text);
         ForegroundColorSpan foregroundColorSpanPurple = new ForegroundColorSpan(Color.rgb(51,116,229));
-
         spannableString.setSpan(foregroundColorSpanPurple, 16, 36, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         mRegisterAccountTextView.setText(spannableString);
     }
+
+
 }
---
+++ app/src/main/java/com/example/dozetracker/ui/SignUpScreen.java
@@ -0,0 +1,16 @@
+package com.example.dozetracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dozetracker.R;
+
+public class SignUpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_screen);
+    }
+}
