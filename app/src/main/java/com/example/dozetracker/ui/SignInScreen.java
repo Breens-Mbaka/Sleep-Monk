dee2c3438d39de8b57ebadb720c7e5f4aeddda63
Breens-Thomas <mbakabreens@gmail.com>
2021-06-30 14:39:28 +0300

    Binded the views in the sign in layout for reference

--- app/src/main/java/com/example/dozetracker/ui/SignInScreen.java
+++ app/src/main/java/com/example/dozetracker/ui/SignInScreen.java
@@ -10,26 +10,36 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.dozetracker.R;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignInScreen extends AppCompatActivity {
     @BindView(R.id.textView10) TextView mRegisterAccountTextView;
+    @BindView(R.id.emailInput) EditText mEmailInputEditText;
+    @BindView(R.id.passwordInput) EditText mPasswordInputEditText;
+    @BindView(R.id.signInUser) Button mSignInButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
-
         ButterKnife.bind(this);
+
         changePartOfTextViewColor();
         registerAccount();
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void registerAccount() {
