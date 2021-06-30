fb03f1eb1fc0376c9904950379c9b1eee0803ff2
Breens-Thomas <mbakabreens@gmail.com>
2021-06-30 09:15:54 +0300

    Override the Onclick method to listen to clicks for certain views in the layout

--- app/src/main/java/com/example/dozetracker/ui/SignUpScreen.java
+++ app/src/main/java/com/example/dozetracker/ui/SignUpScreen.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,7 +18,7 @@ import com.example.dozetracker.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignUpScreen extends AppCompatActivity {
+public class SignUpScreen extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.textView10) TextView mSignInTextView;
     @BindView(R.id.emailTextInput) EditText mEmailEditText;
     @BindView(R.id.passwordTextInput) EditText mPasswordEditText;
@@ -28,10 +29,10 @@ public class SignUpScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
-
         ButterKnife.bind(this);
+
+        mSignInTextView.setOnClickListener(this);
         changePartOfTextViewColor();
-        signInToAccount();
     }
 
     private void changePartOfTextViewColor() {
@@ -42,10 +43,11 @@ public class SignUpScreen extends AppCompatActivity {
         mSignInTextView.setText(spannableString);
     }
 
-    private void signInToAccount() {
-        mSignInTextView.setOnClickListener(v -> {
+    @Override
+    public void onClick(View view) {
+        if(view == mSignInTextView) {
             Intent intent = new Intent(SignUpScreen.this,SignInScreen.class);
             startActivity(intent);
-        });
+        }
     }
 }
